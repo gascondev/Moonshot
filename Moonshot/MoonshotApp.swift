70e13d6bf1b8d7ffc12d4fd973568c0c41f02e09
Álvaro Gascón <alvarogascon@hotmail.com>
2024-05-18 11:28:47 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Álvaro Gascón on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
