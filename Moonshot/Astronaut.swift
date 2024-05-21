fb74a92b7d2dc20ab72c1836cc9f855e74ee2f06
Álvaro Gascón <alvarogascon@hotmail.com>
2024-05-21 06:55:50 +0200

    Missions list created

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Álvaro Gascón on 20/5/24.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,40 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Álvaro Gascón on 20/5/24.
+//
+
+import Foundation
+
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locale \(file) in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' - \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_ , let context) {
+            fatalError("Failed to decode \(file) from bundle due to type masmatch - \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+}
